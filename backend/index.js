9d9102697787189aa6fd1232ee84f213dae69523
VishaalPillay <vishaalpillay18@gmail.com>
2025-06-09 20:25:43 +0530

    Added Test working Express Server

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const express = require('express');
+const app = express();
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Bus Tracking Backend Running!');
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
